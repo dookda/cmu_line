a0648c51b769e937744fd51c2594c8159e3db774
dookda <sakda.homhuan@gmail.com>
2022-03-28 20:18:30 +0700

    update bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './Assets/css/style.css';
-import './App.css';
+// import './Assets/css/style.css';
+// import './App.css';
 import Nav from './Nav/Nav';
 import Card from './Card/Card';
 
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './../Assets/css/style.css';
 function Nav() {
     return (
         <>
